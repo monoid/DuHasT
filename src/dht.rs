b86d2911ac9778b00920c0cd10ffd43e30efa5f8
Ivan Boldyrev <lispnik@gmail.com>
2020-03-29 18:51:08 +0300

    Buggy version with serde_bencode.

---
+++ src/dht.rs
@@ -0,0 +1,64 @@
+extern crate rand_core;
+extern crate rand_chacha;
+extern crate serde_bencode;
+
+use std::fs::File;
+use std::io::{Read, Write};
+
+use rand::{CryptoRng, SeedableRng, Rng, RngCore};
+use rand::rngs::OsRng;
+use rand_chacha::ChaCha20Rng;
+
+
+const DHT_ID_BYTE_SIZE: usize = 160/8;
+pub(crate) const DEFAULT_STATE_PATH: &'static str = "duhast.state";
+
+type KeyBuf = [u8; 160/8];
+
+#[derive(Deserialize, Serialize)]
+pub(crate) struct DhtId {
+    buf: KeyBuf,
+}
+
+impl DhtId {
+    pub(crate) fn new<R: Rng + CryptoRng>(rng: &mut R) -> Self {
+        let mut buf: KeyBuf = Default::default();
+        rng.fill(&mut buf);
+        DhtId { buf }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+pub(crate) struct Config {
+    dht_id: DhtId,
+    peers: Vec<String>,  // String is a stub here.
+}
+
+impl Config {
+    pub(crate) fn new<R: Rng + CryptoRng>(rng: &mut R) -> Self {
+        Config {
+            dht_id: DhtId::new(rng),
+            peers: vec![],
+        }
+    }
+
+    pub(crate) fn load(filename: &str) -> Result<Config, serde_bencode::Error> {
+        let mut file = File::open(filename).unwrap();
+        let mut config_data = vec![];
+        file.read_to_end(&mut config_data).unwrap();
+        serde_bencode::from_bytes::<Config>(&config_data)
+    }
+
+    pub(crate) fn write(&self, filename: &str) -> Result<(), serde_bencode::Error> {
+        let config_data = serde_bencode::to_bytes(self)?;
+        let mut file = File::create(filename).unwrap();
+        file.write(&config_data).unwrap();
+        Ok(())
+    }
+}
+
+pub(crate) fn init_chacha() -> ChaCha20Rng {
+    let mut random: <ChaCha20Rng as SeedableRng>::Seed = Default::default();
+    OsRng.fill_bytes(&mut random);
+    ChaCha20Rng::from_seed(random)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate static_assertions;
+#[macro_use]
+extern crate serde_derive;
+
+use std::path::Path;
+
+mod dht;
+
+
 fn main() {
+    let mut chacha = dht::init_chacha();
+    let cfg = if Path::new(dht::DEFAULT_STATE_PATH).exists() {
+        dht::Config::load(dht::DEFAULT_STATE_PATH).unwrap()
+    } else {
+        dht::Config::new(&mut chacha)
+    };
+    cfg.write(dht::DEFAULT_STATE_PATH).unwrap();
     println!("Hello, world!");
 }

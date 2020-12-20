56c3bfa67397eeb24ca0d18342c2033e2b7d64a1
Ivan Boldyrev <lispnik@gmail.com>
2020-12-20 22:08:39 +0300

    Init Chacha only on config creation
    Thus OS random source is used only when really
    needed.

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use std::path::Path;
 mod dht;
 
 fn main() {
-    let mut chacha = dht::init_chacha();
     let cfg = if Path::new(dht::DEFAULT_STATE_PATH).exists() {
         dht::Config::load(dht::DEFAULT_STATE_PATH).unwrap()
     } else {
+        let mut chacha = dht::init_chacha();
         dht::Config::new(&mut chacha)
     };
     cfg.write(dht::DEFAULT_STATE_PATH).unwrap();

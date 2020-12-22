de21675bd1744b8b9707203bd57c563dcfe2112c
Ivan Boldyrev <lispnik@gmail.com>
2020-12-22 17:34:03 +0300

    Declare serde DHT messages types
    Test are incomplete and do not pass, so it is rather a sketch.

--- src/dht.rs
+++ src/dht.rs
@@ -5,11 +5,9 @@ extern crate serde_bencode;
 use std::fmt;
 use std::fs::File;
 use std::io::{Read, Write};
-use std::net::Ipv4Addr;
 use std::net::SocketAddrV4;
 
-use byteorder::{ByteOrder, NetworkEndian};
-
+use fmt::Debug;
 use rand::rngs::OsRng;
 use rand::{CryptoRng, Rng, RngCore, SeedableRng};
 use rand_chacha::ChaCha20Rng;
@@ -42,6 +40,12 @@ impl DhtId {
     }
 }
 
+impl Debug for DhtId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("{dht_id}")
+    }
+}
+
 // Packed representation of DHT ID and ipv4/port pair.
 pub(crate) struct DhtContactId(ContactIdBuf);
 
@@ -58,6 +62,12 @@ impl DhtContactId {
         debug_assert!(slice.is_empty());
 
         DhtContactId(buf)
+    }
+}
+
+impl Debug for DhtContactId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("{DHT_CONTACT_ID}")
     }
 }
 
@@ -132,6 +142,87 @@ impl<'de> Deserialize<'de> for DhtContactId {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct CompactNodeInfo {
+    // TODO
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct PingQuery {
+    pub(crate) id: DhtId,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct FindNodeQuery {
+    id: DhtContactId,
+    target: DhtContactId,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct GetPeersQuery {
+    id: DhtContactId,
+    info_hash: DhtId,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct AnnouncePeerQuery {
+    id: DhtContactId,
+    implied_port: u8,
+    info_hash: DhtId,
+    port: u16,
+    token: String, // TODO Token is a byte string
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(tag = "q", content = "a")]
+pub(crate) enum Query {
+    #[serde(rename = "ping")]
+    Ping(PingQuery),
+    #[serde(rename = "find_node")]
+    FindNode(FindNodeQuery),
+    #[serde(rename = "get_peers")]
+    GetPeers(GetPeersQuery),
+    #[serde(rename = "announce_peer")]
+    AnnouncePeer(AnnouncePeerQuery),
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct PingResponse {
+    id: DhtContactId,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct FindNodeResponse {
+    id: DhtContactId,
+    nodes: CompactNodeInfo,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct GetPeersResponse {
+    id: DhtContactId,
+    token: String, // TODO just bytes
+    values: Option<Vec<DhtContactId>>,
+    nodes: Option<CompactNodeInfo>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct AnnouncePeerResponse {
+    id: DhtContactId,
+}
+
+type ErrorKind = u32;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "y")]
+pub(crate) enum Message<R> {
+    #[serde(rename = "q")]
+    Q(Query),
+    #[serde(rename = "r")]
+    R { r: R },
+    #[serde(rename = "e")]
+    E { e: (ErrorKind, String) },
+}
+
 #[derive(Deserialize, Serialize)]
 pub(crate) struct Config {
     pub(crate) dht_id: DhtId,
@@ -165,4 +256,58 @@ pub(crate) fn init_chacha() -> ChaCha20Rng {
     let mut random: <ChaCha20Rng as SeedableRng>::Seed = Default::default();
     OsRng.fill_bytes(&mut random);
     ChaCha20Rng::from_seed(random)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::error::Error;
+
+    #[test]
+    fn test_unpack_ping_query() -> Result<(), Box<dyn Error>> {
+        // const DATA: &'static [u8] = b"d1:q4:ping1:ad2:id20:abcdefghij0123456789ee";
+        const DATA: [u8; 43] = [
+            100u8, 49, 58, 97, 100, 50, 58, 105, 100, 50, 48, 58, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 101, 49, 58, 113, 52, 58, 112, 105, 110, 103, 101,
+        ];
+        let err: Query = serde_bencode::from_bytes(&DATA)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_unpack_ping_query_1() -> Result<(), Box<dyn Error>> {
+        const DATA: &'static [u8] = b"d2:id20:abcdefghij0123456789e";
+        let err: PingQuery = serde_bencode::from_bytes(DATA)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_unpack_find_node_query() {}
+
+    #[test]
+    fn test_unpack_get_peers_query() {}
+
+    #[test]
+    fn test_unpack_announce_peer_query() {}
+
+    #[test]
+    fn test_unpack_ping_response() {}
+
+    #[test]
+    fn test_unpack_find_node_response() {}
+
+    #[test]
+    fn test_unpack_get_peers_response() {}
+
+    #[test]
+    fn test_unpack_announce_peer_response() {}
+
+    #[test]
+    fn test_unpack_error_response() -> Result<(), Box<dyn Error>> {
+        const DATA: &'static [u8] = b"d1:eli201e23:A Generic Error Ocurrede1:t2:aa1:y1:ee";
+        let err: Message<PingResponse> = serde_bencode::from_bytes(DATA)?;
+
+        assert!(matches!(dbg!(err), Message::E{e: (201, _)}));
+        Ok(())
+    }
 }

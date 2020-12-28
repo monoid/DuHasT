dc5948e39f9a70bc23d48eef95d2ef0100cfe8a8
Ivan Boldyrev <lispnik@gmail.com>
2020-12-28 17:56:43 +0300

    Implement message serialization with patched deps
    Define DHT message format with Serde.  All methods are supported with
    all functionality, except "v" field which is optional (though adding
    such optional field is quite simple).
    
    We use patched serde and serde_bencoded, because "serde does not
    implement internally tagged enums for non-self-describing formats".
    While bencode seem to be self-describing, it does not distinguish
    bytes and strings, and Serde doesn't handle it (though it could).

--- src/dht.rs
+++ src/dht.rs
@@ -2,6 +2,7 @@ extern crate rand_chacha;
 extern crate rand_core;
 extern crate serde_bencoded;
 
+use std::borrow::Cow;
 use std::fmt;
 use std::fs::File;
 use std::io::{Read, Write};
@@ -16,21 +17,15 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 const DHT_ID_BYTE_SIZE: usize = 160 / 8;
 // Standard 4 bytes IPv4 address + 2 bytes port
-const IP_ADDR_BYTE_SIZE: usize = 6;
+const NODE_ADDR_BYTE_SIZE: usize = 6;
 pub(crate) const DEFAULT_STATE_PATH: &'static str = "duhast.state";
 
 type KeyBuf = [u8; DHT_ID_BYTE_SIZE];
-type ContactIdBuf = [u8; DHT_ID_BYTE_SIZE + IP_ADDR_BYTE_SIZE];
+type NodeBuf = [u8; NODE_ADDR_BYTE_SIZE];
+type ContactIdBuf = [u8; DHT_ID_BYTE_SIZE + NODE_ADDR_BYTE_SIZE];
 
-#[derive(Clone, Default)]
+#[derive(Clone, Default, PartialEq, Eq)]
 pub(crate) struct DhtId(KeyBuf);
-
-// Serde doesn't yet call serialize_bytes; call it manually.
-impl Serialize for DhtId {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_bytes(&self.0)
-    }
-}
 
 impl DhtId {
     pub(crate) fn new<R: Rng + CryptoRng>(rng: &mut R) -> Self {
@@ -40,13 +35,62 @@ impl DhtId {
     }
 }
 
+// Serde doesn't yet call serialize_bytes; call it manually.
+impl Serialize for DhtId {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_bytes(&self.0)
+    }
+}
+
 impl Debug for DhtId {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str("{dht_id}")
     }
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub(crate) struct NodeAddr(NodeBuf);
+
+// Serde doesn't yet call serialize_bytes; call it manually.
+impl Serialize for NodeAddr {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_bytes(&self.0)
+    }
+}
+
+struct NodeAddrDeserializerVisitor;
+
+impl<'de> serde::de::Visitor<'de> for NodeAddrDeserializerVisitor {
+    type Value = NodeAddr;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "{} bytes", NODE_ADDR_BYTE_SIZE)
+    }
+
+    fn visit_bytes<E: serde::de::Error>(self, v: &[u8]) -> Result<Self::Value, E> {
+        if v.len() == NODE_ADDR_BYTE_SIZE {
+            let mut buf: NodeBuf = Default::default();
+            &buf.copy_from_slice(&v[..]);
+            Ok(NodeAddr(buf))
+        } else {
+            Err(E::invalid_length(v.len(), &"6 bytes"))
+        }
+    }
+
+    fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
+        self.visit_bytes(v.as_bytes())
+    }
+}
+
+// Serde doesn't yet call serialize_bytes; call it manually.
+impl<'de> Deserialize<'de> for NodeAddr {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        deserializer.deserialize_bytes(NodeAddrDeserializerVisitor)
+    }
+}
+
 // Packed representation of DHT ID and ipv4/port pair.
+#[derive(PartialEq, Eq)]
 pub(crate) struct DhtContactId(ContactIdBuf);
 
 impl DhtContactId {
@@ -95,6 +139,10 @@ impl<'de> serde::de::Visitor<'de> for DhtIdDeserializerVisitor {
             Err(E::invalid_length(v.len(), &"20 bytes"))
         }
     }
+
+    fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
+        self.visit_bytes(v.as_bytes())
+    }
 }
 
 // Serde doesn't yet call serialize_bytes; call it manually.
@@ -113,114 +161,145 @@ impl fmt::Display for DhtId {
     }
 }
 
-// TODO: generalize these two deserializer...
-struct DhtContactIdDeserializerVisitor;
 
-impl<'de> serde::de::Visitor<'de> for DhtContactIdDeserializerVisitor {
-    type Value = DhtContactId;
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) struct CompactNodesList<'msg>(Cow<'msg, [u8]>);
+
+impl<'msg> Serialize for CompactNodesList<'msg> {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_bytes(&self.0)
+    }
+}
+
+struct CompactNodesListDeserializerVisitor;
+
+impl<'de> serde::de::Visitor<'de> for CompactNodesListDeserializerVisitor {
+    type Value = CompactNodesList<'de>;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "{} bytes", std::mem::size_of::<ContactIdBuf>())
+        write!(formatter, "{} bytes", DHT_ID_BYTE_SIZE)
     }
 
-    fn visit_bytes<E: serde::de::Error>(self, v: &[u8]) -> Result<Self::Value, E> {
-        if v.len() == std::mem::size_of::<ContactIdBuf>() {
-            let mut buf: ContactIdBuf = Default::default();
-            &buf.copy_from_slice(&v[..]);
-
-            Ok(DhtContactId(buf))
+    fn visit_borrowed_bytes<E: serde::de::Error>(self, v: &'de [u8]) -> Result<Self::Value, E> {
+        if v.len() % (DHT_ID_BYTE_SIZE + NODE_ADDR_BYTE_SIZE) == 0 {
+            Ok(CompactNodesList(Cow::Borrowed(v)))
         } else {
-            Err(E::invalid_length(v.len(), &"26 bytes"))
+            Err(E::invalid_length(v.len(), &"divisible by 26 bytes"))
+        }
+    }
+
+    fn visit_byte_buf<E: serde::de::Error>(self, v: Vec<u8>) -> Result<Self::Value, E> {
+        if v.len() % (DHT_ID_BYTE_SIZE + NODE_ADDR_BYTE_SIZE) == 0 {
+            Ok(CompactNodesList(Cow::Owned(v)))
+        } else {
+            Err(E::invalid_length(v.len(), &"divisible by 26 bytes"))
         }
     }
 }
 
 // Serde doesn't yet call serialize_bytes; call it manually.
-impl<'de> Deserialize<'de> for DhtContactId {
+impl<'de: 'a, 'a> Deserialize<'de> for CompactNodesList<'a> {
     fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        deserializer.deserialize_bytes(DhtContactIdDeserializerVisitor)
+        deserializer.deserialize_bytes(CompactNodesListDeserializerVisitor)
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub(crate) struct CompactNodeInfo {
-    // TODO
-}
-
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub(crate) struct PingQuery {
     pub(crate) id: DhtId,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub(crate) struct FindNodeQuery {
-    id: DhtContactId,
-    target: DhtContactId,
+    id: DhtId,
+    target: DhtId,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub(crate) struct GetPeersQuery {
-    id: DhtContactId,
+    id: DhtId,
     info_hash: DhtId,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-pub(crate) struct AnnouncePeerQuery {
-    id: DhtContactId,
-    implied_port: u8,
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
+pub(crate) struct AnnouncePeerQuery<'msg> {
+    id: DhtId,
     info_hash: DhtId,
+    #[serde(borrow, with = "serde_bytes")]
+    token: Cow<'msg, [u8]>,
     port: u16,
-    token: String, // TODO Token is a byte string
+    implied_port: u8,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 #[serde(tag = "q", content = "a")]
-pub(crate) enum Query {
+pub(crate) enum Query<'msg> {
     #[serde(rename = "ping")]
     Ping(PingQuery),
     #[serde(rename = "find_node")]
     FindNode(FindNodeQuery),
     #[serde(rename = "get_peers")]
     GetPeers(GetPeersQuery),
-    #[serde(rename = "announce_peer")]
-    AnnouncePeer(AnnouncePeerQuery),
+    #[serde(borrow, rename = "announce_peer")]
+    AnnouncePeer(AnnouncePeerQuery<'msg>),
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub(crate) struct PingResponse {
-    id: DhtContactId,
+    id: DhtId,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-pub(crate) struct FindNodeResponse {
-    id: DhtContactId,
-    nodes: CompactNodeInfo,
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
+pub(crate) struct FindNodeResponse<'msg> {
+    id: DhtId,
+    #[serde(borrow)]
+    nodes: CompactNodesList<'msg>,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-pub(crate) struct GetPeersResponse {
-    id: DhtContactId,
-    token: String, // TODO just bytes
-    values: Option<Vec<DhtContactId>>,
-    nodes: Option<CompactNodeInfo>,
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+pub(crate) struct GetPeersResponse<'msg> {
+    id: DhtId,
+    #[serde(borrow)]
+    token: Cow<'msg, [u8]>,
+    values: Option<Vec<NodeAddr>>,
+    #[serde(borrow)]
+    nodes: Option<CompactNodesList<'msg>>,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub(crate) struct AnnouncePeerResponse {
-    id: DhtContactId,
+    id: DhtId,
 }
 
 type ErrorKind = u32;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 #[serde(tag = "y")]
-pub(crate) enum Message<R> {
-    #[serde(rename = "q")]
-    Q(Query),
+pub(crate) enum Message<'msg, R> {
+    #[serde(borrow, rename = "q")]
+    Q(Query<'msg>),
     #[serde(rename = "r")]
     R { r: R },
     #[serde(rename = "e")]
     E { e: (ErrorKind, String) },
+}
+
+// When `y` is "q", it is an incoming query with new `tt` token.  When
+// `y` is "r" or "e", it is response to old quer with `tt` that should
+// be known, and with this knowledge one can parse it.
+#[derive(Deserialize, Debug, Eq, PartialEq)]
+pub(crate) struct IncomingMessage<'msg> {
+    #[serde(borrow)]
+    y: &'msg str,
+    #[serde(borrow, with = "serde_bytes")]
+    tt: &'msg [u8],
+}
+
+#[derive(Serialize, Debug)]
+pub(crate) struct OutgoingMessage<'msg, R> {
+    tt: Cow<'msg, [u8]>,
+    #[serde(borrow, flatten)]
+    msg: Message<'msg, R>,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -264,47 +343,177 @@ mod tests {
     use std::error::Error;
 
     #[test]
+    fn test_unpack_incoming_msg() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] =
+            b"d1:ad2:id20:\xFFbcdefghij0123456789e1:q4:ping1:t2:aa1:y1:q2:tt2:\xFF\xFFe";
+        let ping: IncomingMessage = serde_bencoded::from_bytes(&DATA)?;
+
+        assert_eq!(
+            ping,
+            IncomingMessage {
+                y: "q",
+                tt: b"\xFF\xFF"
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
     fn test_unpack_ping_query() -> Result<(), Box<dyn Error>> {
-        const DATA: &'static [u8] = b"d1:q4:ping1:ad2:id20:abcdefghij0123456789ee";
-        // const DATA: [u8; 43] = [
-        //     100u8, 49, 58, 97, 100, 50, 58, 105, 100, 50, 48, 58, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        //     0, 0, 0, 0, 0, 0, 0, 0, 0, 101, 49, 58, 113, 52, 58, 112, 105, 110, 103, 101,
-        // ];
-        let err: Query = serde_bencoded::from_bytes(&DATA)?;
+        const DATA: &[u8] =
+            b"d1:ad2:id20:\xFFbcdefghij0123456789e1:q4:ping1:t2:aa1:y1:q2:tt2:\xFF\xFFe";
+        let ping: Message<()> = serde_bencoded::from_bytes(&DATA)?;
+
+        assert_eq!(
+            ping,
+            Message::Q(Query::Ping(PingQuery {
+                id: DhtId(b"\xFFbcdefghij0123456789".clone())
+            }))
+        );
         Ok(())
     }
 
     #[test]
-    fn test_unpack_ping_query_1() -> Result<(), Box<dyn Error>> {
-        const DATA: &'static [u8] = b"d2:id20:abcdefghij0123456789e";
-        let err: PingQuery = serde_bencoded::from_bytes(DATA)?;
+    fn test_unpack_find_node_query() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] = b"d1:ad2:id20:abcdefghij01234567896:target20:mnopqrstuvwxyz123456e1:q9:find_node1:t2:aa1:y1:qe";
+        let find_node: Message<()> = serde_bencoded::from_bytes(&DATA)?;
+
+        assert_eq!(
+            find_node,
+            Message::Q(Query::FindNode(FindNodeQuery {
+                id: DhtId(b"abcdefghij0123456789".clone()),
+                target: DhtId(b"mnopqrstuvwxyz123456".clone()),
+            }))
+        );
         Ok(())
     }
 
     #[test]
-    fn test_unpack_find_node_query() {}
+    fn test_unpack_get_peers_query() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] = b"d1:ad2:id20:abcdefghij01234567899:info_hash20:mnopqrstuvwxyz123456e1:q9:get_peers1:t2:aa1:y1:qe";
+        let get_peers: Message<()> = serde_bencoded::from_bytes(&DATA)?;
+        assert_eq!(
+            get_peers,
+            Message::Q(Query::GetPeers(GetPeersQuery {
+                id: DhtId(b"abcdefghij0123456789".clone()),
+                info_hash: DhtId(b"mnopqrstuvwxyz123456".clone()),
+            }))
+        );
+        Ok(())
+    }
 
     #[test]
-    fn test_unpack_get_peers_query() {}
+    fn test_unpack_announce_peer_query() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] = b"d1:ad2:id20:abcdefghij012345678912:implied_porti1e9:info_hash20:mnopqrstuvwxyz1234564:porti6881e5:token8:aoeusnthe1:q13:announce_peer1:t2:aa1:y1:qe";
+        let announce_peer: Message<()> = serde_bencoded::from_bytes(&DATA)?;
+        assert_eq!(
+            announce_peer,
+            Message::Q(Query::AnnouncePeer(AnnouncePeerQuery {
+                id: DhtId(b"abcdefghij0123456789".clone()),
+                implied_port: 1,
+                info_hash: DhtId(b"mnopqrstuvwxyz123456".clone()),
+                port: 6881,
+                token: Cow::Borrowed(b"aoeusnth")
+            }))
+        );
+        Ok(())
+    }
 
     #[test]
-    fn test_unpack_announce_peer_query() {}
+    fn test_unpack_ping_response() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] = b"d1:rd2:id20:mnopqrstuvwxyz123456e1:t2:aa1:y1:re";
+        let ping: Message<PingResponse> = serde_bencoded::from_bytes(&DATA)?;
+        assert_eq!(
+            ping,
+            Message::R {
+                r: PingResponse {
+                    id: DhtId(b"mnopqrstuvwxyz123456".clone()),
+                }
+            }
+        );
+        Ok(())
+    }
 
     #[test]
-    fn test_unpack_ping_response() {}
+    fn test_unpack_find_node_response() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] =
+            b"d1:rd2:id20:0123456789abcdefghij5:nodes26:01234567890123456789abcdefe1:t2:aa1:y1:re";
+        let find_node: Message<FindNodeResponse> = serde_bencoded::from_bytes(&DATA)?;
+        assert_eq!(
+            find_node,
+            Message::R {
+                r: FindNodeResponse {
+                    id: DhtId(b"0123456789abcdefghij".clone()),
+                    nodes: CompactNodesList(Cow::Owned(Vec::from(
+                        b"01234567890123456789abcdef".clone()
+                    )))
+                }
+            }
+        );
+        Ok(())
+    }
 
     #[test]
-    fn test_unpack_find_node_response() {}
+    fn test_unpack_get_peers_response_values() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] =
+    b"d1:rd2:id20:abcdefghij01234567895:token8:aoeusnth6:valuesl6:axje.u6:idhtnmee1:t2:aa1:y1:re";
+        let get_peers: Message<GetPeersResponse> = serde_bencoded::from_bytes(DATA)?;
+        assert_eq!(
+            get_peers,
+            Message::R {
+                r: GetPeersResponse {
+                    id: DhtId(b"abcdefghij0123456789".clone()),
+                    token: Cow::Borrowed(b"aoeusnth"),
+                    values: Some(vec![
+                        NodeAddr(b"axje.u".clone()),
+                        NodeAddr(b"idhtnm".clone())
+                    ]),
+                    nodes: None,
+                }
+            }
+        );
+        Ok(())
+    }
 
     #[test]
-    fn test_unpack_get_peers_response() {}
+    fn test_unpack_get_peers_response_nodes() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] =
+    b"d1:rd2:id20:abcdefghij01234567895:token8:aoeusnth5:nodes26:01234567890123456789012345e1:t2:aa1:y1:re";
+        let get_peers: Message<GetPeersResponse> = serde_bencoded::from_bytes(DATA)?;
+        assert_eq!(
+            get_peers,
+            Message::R {
+                r: GetPeersResponse {
+                    id: DhtId(b"abcdefghij0123456789".clone()),
+                    token: Cow::Borrowed(b"aoeusnth"),
+                    values: None,
+                    nodes: Some(CompactNodesList(Cow::Owned(Vec::from(
+                        b"01234567890123456789012345".clone()
+                    )))),
+                }
+            }
+        );
+        Ok(())
+    }
 
     #[test]
-    fn test_unpack_announce_peer_response() {}
+    fn test_unpack_announce_peer_response() -> Result<(), Box<dyn Error>> {
+        const DATA: &[u8] = b"d1:rd2:id20:mnopqrstuvwxyz123456e1:t2:aa1:y1:re";
+        let announce_peers: Message<AnnouncePeerResponse> = serde_bencoded::from_bytes(DATA)?;
+        assert_eq!(
+            announce_peers,
+            Message::R {
+                r: AnnouncePeerResponse {
+                    id: DhtId(b"mnopqrstuvwxyz123456".clone()),
+                }
+            }
+        );
+        Ok(())
+    }
 
     #[test]
     fn test_unpack_error_response() -> Result<(), Box<dyn Error>> {
-        const DATA: &'static [u8] = b"d1:eli201e23:A Generic Error Ocurrede1:t2:aa1:y1:ee";
+        const DATA: &[u8] = b"d1:eli201e23:A Generic Error Ocurrede1:t2:aa1:y1:ee";
         let err: Message<PingResponse> = serde_bencoded::from_bytes(DATA)?;
 
         assert!(matches!(dbg!(err), Message::E{e: (201, _)}));

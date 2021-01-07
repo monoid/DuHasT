0b7db307ebf1fb9dec1767086863338c4d76ab60
Ivan Boldyrev <lispnik@gmail.com>
2021-01-07 19:36:48 +0300

    Implement bep 0042 with the arrayvec

--- src/bep_0042.rs
+++ src/bep_0042.rs
@@ -1,25 +1,27 @@
-/// Implements https://www.bittorrent.org/beps/bep_0042.html
-use std::net::IpAddr;
 use crate::dht::DhtId;
-use rand::{CryptoRng, Rng};
 use crc32c_hw;
+use rand::{CryptoRng, Rng};
+use std::net::IpAddr;
+use arrayvec::ArrayVec;
+
 
 pub(crate) fn get_crc(ip: IpAddr, r: u8) -> u32 {
     match ip {
         IpAddr::V4(v4) => {
-            let mut masked: Vec<u8> = [0x03u8, 0x0f, 0x3f, 0xff].iter().zip(&v4.octets()).map(|(a, b)| a & b).collect();
+            let mut masked: ArrayVec<[u8; 4]> = [0x03u8, 0x0f, 0x3f, 0xff].iter().zip(&v4.octets()).map(|(a, b)| a & b).collect();
             masked[0] |= r << 5;
-            
-            dbg!(crc32c_hw::compute(dbg!(&masked)))
+
+            crc32c_hw::compute(masked)
         }
         IpAddr::V6(v6) => {
-            let mut masked: Vec<u8> = [0x01u8, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff].iter().zip(&v6.octets()).map(|(a, b)| a & b).collect();
+            let mut masked: ArrayVec<[u8; 8]> = [0x01u8, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff].iter().zip(&v6.octets()).map(|(a, b)| a & b).collect();
             masked[0] |= r << 5;
             crc32c_hw::compute(&masked)
         }
     }
 }
 
+/// Implements https://www.bittorrent.org/beps/bep_0042.html
 pub(crate) fn gen_self_id<R: Rng + CryptoRng>(self_ip: IpAddr, rng: &mut R) -> DhtId {
     // We waste some bytes of random data, but this func is used rarely.
     let mut original = DhtId::new(rng);
@@ -33,7 +35,6 @@ pub(crate) fn gen_self_id<R: Rng + CryptoRng>(self_ip: IpAddr, rng: &mut R) -> D
     original
 }
 
-
 #[cfg(test)]
 mod test {
     use std::net::IpAddr;
@@ -41,15 +42,17 @@ mod test {
     #[test]
     fn test_spec() {
         // 2797153130
-        
+
         // Single test from the spec.
         let d = crate::dht::DhtId::from_str("5fbfbff10c5d6a4ec8a88e4c6ab4c28b95eee401").unwrap();
         let ip: IpAddr = [124, 31, 75, 21].into();
-        
+
         let crc = super::get_crc(ip, d.0[19]);
         assert_eq!(crc >> 11, 0x05fbfbf >> 3);
         let crc = crc.to_be_bytes();
-        assert_eq!([crc[0], crc[1], crc[2] & 0xF8],
-                   [d.0[0], d.0[1], d.0[2] & 0xF8]);
+        assert_eq!(
+            [crc[0], crc[1], crc[2] & 0xF8],
+            [d.0[0], d.0[1], d.0[2] & 0xF8]
+        );
     }
 }
